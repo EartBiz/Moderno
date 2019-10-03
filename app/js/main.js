35aa64c76b26a8bfd1b0595766ebcde23187ecbb
EartBiz <bmw.5.fan@gmail.com>
2019-10-04 00:37:04 +0300

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -15,6 +15,26 @@ $('.product-slider__inner').slick({
 
 });
 
+$(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
+});
+
+$('.icon-th-list').on('click', function(){
+    $('.product__item').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon-th-large').removeClass('active');
+;});
+$('.icon-th-large').on('click', function(){
+    $('.product__item').removeClass('list');
+    $('.icon-th-large').addClass('active');
+    $('.icon-th-list').removeClass('active');
+});
+
 var mixer = mixitup('.products__inner-box');
 
 });
