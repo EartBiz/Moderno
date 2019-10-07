e037587f6145a90cf0fa5b0fbf9ee2b4adf4e346
EartBiz <bmw.5.fan@gmail.com>
2019-10-07 18:37:07 +0300

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -35,6 +35,14 @@ $('.icon-th-large').on('click', function(){
     $('.icon-th-list').removeClass('active');
 });
 
+$('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+});
+
+$('.header__btn-menu').on('click', function(){
+    $('.header__box').toggleClass('active');
+});
+
 var mixer = mixitup('.products__inner-box');
 
 });
